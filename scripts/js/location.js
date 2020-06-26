1b77970d46390452ab509d14f5e7eab5d0550605
Dmitry Berestnev <dimka@bdima.ru>
2020-06-26 16:10:55 +0000

    pop economy

--- scripts/js/location.js
+++ scripts/js/location.js
@@ -8,9 +8,6 @@ $(document).ready(function(){
     }else if(url == "for_investors"){
         $("#sidebarMenu > div > ul > li:nth-of-type(3) span")[0].click();
         $("#sidebarMenu > div > ul > li:nth-of-type(3) a")[0].click();
-    }else if(url == "economy"){
-        $("#sidebarMenu > div > ul > li:nth-of-type(2) span")[0].click();
-        $("#sidebarMenu > div > ul > li:nth-of-type(2) a")[0].click();
     }
 });
 /*
